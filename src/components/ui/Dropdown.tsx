2bde2be6ca19c140f4f32ca50e465d95c0a24b96
miragitting <m_gilespufahl@coloradocollege.edu>
2024-12-03 16:46:44 -0700

    Improved Header layout. Tying to implement a dropdown component for Options.

---
+++ src/components/ui/Dropdown.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Link from 'next/link';
+
+import '@/app/globals.css';
+
+export const Dropdown = () => {
+    const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleMenuOne = () => {
+    // do something
+    setOpen(false);
+  };
+
+  const handleMenuTwo = () => {
+    // do something
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={handleOpen}>Options</button>
+      {open ? (
+        <ul className="menu">
+          <li className="menu-item">
+            <button onClick={handleMenuOne}>Map</button>
+          </li>
+          <li className="menu-item">
+            <button onClick={handleMenuTwo}>Histogram</button>
+          </li>
+        </ul>
+      ) : null}
+      {/*open ? <div>Is Open</div> : <div>Is Closed</div>*/}
+    </div>
+  );
+};
+
