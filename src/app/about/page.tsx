9c4c614ad8b5ec35f4d44d02ced66c8f0cccebfd
“DanSchmidt” <dan@danschmidtmedia.co>
2024-12-09 10:12:19 -0700

    how to push to site example

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
     <div className="min-h-screen bg-white text-gray-900">
         <Header />
         <div className="py-4 px-14 ">
-          <h1 className="text-3xl font-serif">About This Project</h1>
+          <h1 className="text-3xl font-serif">About This Crazy Project</h1>
           <p className="text-sm">Who is this for and why was it created?</p>
           <hr className="h-px my-4 bg-violet-300 border-0"></hr>
         </div>
