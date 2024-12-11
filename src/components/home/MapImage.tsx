e69b88673bf947ab27f08dfb4466f401e03f4d1b
“DanSchmidt” <dan@danschmidtmedia.co>
2024-12-11 14:37:11 -0700

    Custom slugs working w new project structure

--- src/components/home/MapImage.tsx
+++ src/components/home/MapImage.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export const MapImage = () => {
     return (
         <div className="bg-violet-100 border-b shadow-md py-4 px-8 content-between" style={{ borderBottomRightRadius: 10, borderBottomLeftRadius: 10, borderTopLeftRadius: 10, borderTopRightRadius: 10}}>
-            <Link href="/map_page" className="text-xl font-bold font-serif hover:underline">
+            <Link href="/map" className="text-xl font-bold font-serif hover:underline">
             County Level Pass/Fail Density Map
           </Link>
 
