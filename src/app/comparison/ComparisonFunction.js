1621cf695721d94542497a31e7ea58fd9016f044
oliverRamirez4 <oliverramirez307@gmail.com>
2024-12-17 13:23:08 -0700

    Mira meta data refactor

---
+++ src/app/comparison/ComparisonFunction.js
@@ -0,0 +1,12 @@
+'use client'
+import React, { useContext} from 'react';
+import { prop1voteDataContext, prop2voteDataContext } from "./layout";
+import { ComparisonVisual } from '@/components/visuals/comparison';
+
+export default function ComparisonFunction (){
+    const prop1voteData = useContext(prop1voteDataContext);
+    const prop2voteData = useContext(prop2voteDataContext);
+    return (
+        <ComparisonVisual prop1VoteData={prop1voteData} prop2VoteData={prop2voteData}/>
+    )
+}
--- src/app/comparison/page.tsx
+++ src/app/comparison/page.tsx
@@ -1,12 +1,29 @@
-'use client'
-import React, { useContext} from 'react';
-import { prop1voteDataContext, prop2voteDataContext } from "./layout";
-import { ComparisonVisual } from '@/components/visuals/comparison';
+//'use client';
+//import { ColoradoMap } from '@/components/map/D3Map';
+import React from 'react';
+//import { VoteDataContext } from "../layout";
+import ComparisonFunction from '@/app/comparison/ComparisonFunction';
+
+export const metadata = {
+    title: 'Colorado Vote Visualizer',
+    description: 'Understanding how votes add up matters. It matters for big races, but it matters for smaller, down ballot races too.',
+    // open graph is the preview that shows up when the site is shared
+    openGraph: {
+      title: 'Colorado Vote Visualizer: County Map',
+      description: 'Compare the percentages of people voting for propositions in each county.',
+      url: 'https://www.coloradovotevisuals.com/comparison',
+      images: [
+        {
+          url: 'https://coloradocotevisuals.com/preview-image.png',
+          width: 800,
+          height: 600,
+        },
+      ],
+    },
+  };
 
 export default function Map (){
-    const prop1voteData = useContext(prop1voteDataContext);
-    const prop2voteData = useContext(prop2voteDataContext);
-    return (
-        <ComparisonVisual prop1VoteData={prop1voteData} prop2VoteData={prop2voteData}/>
-    )
+  return(
+    <ComparisonFunction/>
+  ) 
 }
