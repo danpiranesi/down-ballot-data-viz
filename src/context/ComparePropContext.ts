95c58d621503d91097a94631a28c104436908dfc
Dan Schmidt <110695726+danpiranesi@users.noreply.github.com>
2024-12-18 23:11:45 -0700

    week 4 git update - dan
    This week I worked on merging everyone's updates into a cohesive whole (that ideally deploys on vercel 🤞) and ensuring the export functionality I had been building in the latter half of week 3 would work with all this new componentry. We managed to get everything working while locally hosted but ran into a lot of type safety errors when trying to deploy to our production site with Vercel. We'll have it up and running by block 7!

--- src/context/ComparePropContext.ts
+++ src/context/ComparePropContext.ts
@@ -1,4 +1,4 @@
 import React, { createContext } from 'react';
 import { Proposition } from '@/types/propdata';
 
-export const ComparePropContext = createContext<Proposition| null>(null);
+export const ComparePropContext = createContext<Proposition| null>(null);  
